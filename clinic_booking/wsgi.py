4acee31c57c7ef85c51d440899fc8b640298736b
Chidera001-dev <nnamanichideraemmanuel7@gmail.com>
2025-10-22 17:39:31 +0100

    feat: update models to use ShortUUID for primary keys and configure WSGI for deployment

--- clinic_booking/wsgi.py
+++ clinic_booking/wsgi.py
@@ -1,16 +1,18 @@
-"""
-WSGI config for clinic_booking project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
 import os
+import sys
+
+# Add project folder to sys.path
+path = '/home/chidera01/doctor_appointment_system'
+if path not in sys.path:
+    sys.path.append(path)
+
+# Set Django settings
+os.environ['DJANGO_SETTINGS_MODULE'] = 'clinic_booking.settings'
+
+# Activate virtualenv (for PythonAnywhere)
+activate_env = '/home/chidera01/.virtualenvs/healthcare-virtualenv/bin/activate_this.py'
+
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "clinic_booking.settings")
-
 application = get_wsgi_application()
+
